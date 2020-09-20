82a1c3bd60c8929e4347071194439b23523796fb
Danila Vlasov <vfked0d@gmail.com>
2020-09-20 18:49:15 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import spiking_network_learning_algorithm
 
 
 setup(
-    name='spiking_network_learning_algorithm',
+    name='spilearn',
     version=spiking_network_learning_algorithm.__version__,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
