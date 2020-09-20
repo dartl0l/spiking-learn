607f93d02ee1baba4a6038a14d376fc818bd26b4
Danila Vlasov <vfked0d@gmail.com>
2020-09-20 18:57:22 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
-import spiking_network_learning_algorithm
+import spilearn
 
 
 setup(
     name='spilearn',
-    version=spiking_network_learning_algorithm.__version__,
+    version=spilearn.__version__,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
 )
