77fdee6c42ecd1230c66fc286defbf1038260ff2
dartl0l <vfked0d@gmail.com>
2023-09-18 00:16:00 +0300

    fix temporal transformer

--- spilearn/estimators.py
+++ spilearn/estimators.py
@@ -176,9 +176,10 @@ class ReceptiveFieldsTransformer(BaseEstimator, TransformerMixin):
 
 
 class TemporalPatternTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, pattern_length, k_round=2, reshape=True, reverse=False, no_last=False) -> None:
+    def __init__(self, pattern_length, k_round=2, max_x=1.0, reshape=True, reverse=False, no_last=False) -> None:
         self.pattern_length = pattern_length
         self.k_round = k_round
+        self.max_x = max_x
         self.reshape = reshape
         self.reverse = reverse
         self.no_last = no_last
@@ -190,7 +191,7 @@ class TemporalPatternTransformer(BaseEstimator, TransformerMixin):
         if self.no_last:
             X[X == 0] = np.nan
 
-        X = 1 - X if self.reverse else X
+        X = X if self.reverse else self.max_x - X
         X = np.round(self.pattern_length * X, self.k_round)
         return X.reshape(X.shape[0], X.shape[1], 1) if self.reshape else X.reshape(X.shape[0], X.shape[1])
 
